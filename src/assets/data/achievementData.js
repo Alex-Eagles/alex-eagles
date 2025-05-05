0da4a204d7a0c6017c8289c9bbf086ae8806c2c4
Sara Gharib <sarayassergharib@gmail.com>
2025-05-05 18:16:58 +0300

    Updated the history page

--- src/assets/data/achievementData.js
+++ src/assets/data/achievementData.js
@@ -1,3 +1,5 @@
+import uavc2024 from "../images/uavc-2024.jpg";
+import suas2023 from "../images/suas-2023.jpg";
 import sae20221 from "../images/sae-2022-1.jpeg";
 import sae20222 from "../images/sae-2022-2.jpeg";
 import sae2021 from "../images/sae-2021.png";
@@ -7,6 +9,27 @@ import sae2018 from "../images/sae-2018.png";
 import sae2017 from "../images/sae-2017.png";
 
 const achievementData = [
+	
+	{
+		achievements: [
+			{
+				title: "5th Place",
+				competition: "UAVC",
+				images: [uavc2024],
+			},
+		],
+		year: "2024",
+	},
+	{
+		achievements: [
+			{
+				title: "11th Place - Overall",
+				competition: "SAE Aero Design",
+				images: [suas2023],
+			},
+		],
+		year: "2023",
+	},
 	{
 		achievements: [
 			{
