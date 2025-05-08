2526498b9c741005aac5db6b74b6dd930c4c0bba
John Ayman <139935256+JohnAyman12@users.noreply.github.com>
2025-05-08 23:50:38 +0300

    shuffling member images each seconds animation

--- src/assets/data/teamMemberData.js
+++ src/assets/data/teamMemberData.js
@@ -1,164 +1,43 @@
-const teamMemberData = [
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-	{
-		name: "Amanda",
-		role: "Co-Founder",
-		image: "https://i.pravatar.cc/300",
-		email: "",
-		linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/",
-		gitHubLink: "",
-	},
-];
+import person1 from '../persons/person_1.png';
+import person2 from '../persons/person_2.png';
+import person3 from '../persons/person_3.png';
+import person4 from '../persons/person_4.png';
+import person5 from '../persons/person_5.png';
+import person6 from '../persons/person_6.png';
+import person7 from '../persons/person_7.png';
+import person8 from '../persons/person_8.png';
+import person9 from '../persons/person_9.png';
+import person10 from '../persons/person_10.png';
+import person11 from '../persons/person_11.png';
+import person12 from '../persons/person_12.png';
+import person13 from '../persons/person_13.png';
+import person14 from '../persons/person_14.png';
+import person15 from '../persons/person_15.png';
+import person16 from '../persons/person_16.png';
+import person17 from '../persons/person_17.png';
+import person18 from '../persons/person_18.png';
+import person19 from '../persons/person_19.png';
+import person20 from '../persons/person_20.png';
 
-export default teamMemberData;
+export const teamMemberData = [
+  { name: "Amanda", role: "Co-Founder1", image: person1, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder2", image: person2, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder3", image: person3, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder4", image: person4, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder5", image: person5, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder6", image: person6, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder7", image: person7, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder8", image: person8, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder9", image: person9, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder10", image: person10, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder11", image: person11, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder12", image: person12, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder13", image: person13, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder14", image: person14, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder15", image: person15, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder16", image: person16, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder17", image: person17, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder18", image: person18, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder19", image: person19, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+  { name: "Amanda", role: "Co-Founder20", image: person20, email: "", linkedInLink: "https://www.linkedin.com/in/amanda-chen-9a9b1a1b6/", gitHubLink: "" },
+];
--- src/components/TeamPage/TeamMembersSection.js
+++ src/components/TeamPage/TeamMembersSection.js
@@ -1,51 +1,89 @@
-import { Box, Container, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Container } from "@mui/material";
+import { motion, AnimatePresence } from "framer-motion";
 import TeamMemberCard from "./TeamMemberCard";
-import teamMemberData from "../../assets/data/teamMemberData";
+import { teamMemberData as initialData } from "../../assets/data/teamMemberData";
 import SectionHeading from "../SectionHeading/SectionHeading";
 
+const MotionBox = motion(Box);
+
 const TeamMembersSection = () => {
+	const [data, setData] = useState([...initialData]);
+	const [swappedIndices, setSwappedIndices] = useState(null); // Track swapped indices
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			// Randomly select two indices
+			const i = Math.floor(Math.random() * data.length);
+			let j = Math.floor(Math.random() * data.length);
+			while (j === i) {
+				j = Math.floor(Math.random() * data.length);
+			}
+
+			// Save swapped indices to state
+			setSwappedIndices([i, j]);
+
+			// Swap elements
+			setData(prevData => {
+				const newData = [...prevData];
+				[newData[i], newData[j]] = [newData[j], newData[i]];
+				return newData;
+			});
+		}, 10000);
+
+		return () => clearInterval(interval);
+	}, [data]);
+
 	return (
-		<Container maxWidth="falsw">
+		<Container maxWidth="false">
 			<SectionHeading
 				title="Our Team"
 				subtitle="Meet the team who make the magic happen."
 			/>
-			<Box
+			<MotionBox
 				sx={{
-					// mt: 4,
 					display: "flex",
 					flexWrap: "wrap",
 					flexDirection: "row",
-					justifyContent:
-						window.innerWidth > 600 ? "center" : "space-between",
+					justifyContent: window.innerWidth > 600 ? "center" : "space-between",
 				}}>
-				{/* TODO: Remove after adding team member data */}
-				{/* <Typography
-					variant="h5"
-					component="span"
-					color="primary"
-					sx={{
-						textAlign: "center",
-						width: "100%",
-						my: 8,
-					}}>
-					Coming Soon!
-				</Typography> */}
-				{/* TODO: Remove after adding team member data */}
-				{teamMemberData.map((member, index) => {
-					return (
-						<TeamMemberCard
-							key={index}
-							name={member.name}
-							role={member.role}
-							image={member.image}
-							email={member.email}
-							linkedInLink={member.linkedInLink}
-							gitHubLink={member.gitHubLink}
-						/>
-					);
-				})}
-			</Box>
+				<AnimatePresence>
+					{data.map((member, index) => {
+						const isSwapped = swappedIndices && (swappedIndices[0] === index || swappedIndices[1] === index);
+						const initialAnimProps = isSwapped
+							? { opacity: 0, scale: 0.8 }
+							: { opacity: 1, scale: 1 };
+
+						const animateAnimProps = isSwapped
+							? { opacity: 1, scale: 1 }
+							: { opacity: 1, scale: 1 };
+
+						const exitAnimProps = isSwapped
+							? { opacity: 0, scale: 0.8 }
+							: { opacity: 1, scale: 1 };
+
+						return (
+							<motion.div
+								key={member.name + member.role + index}
+								layout
+								initial={initialAnimProps}
+								animate={animateAnimProps}
+								exit={exitAnimProps}
+								transition={{ duration: 0.5 }}
+							>
+								<TeamMemberCard
+									name={member.name}
+									role={member.role}
+									image={member.image}
+									email={member.email}
+									linkedInLink={member.linkedInLink}
+									gitHubLink={member.gitHubLink}
+								/>
+							</motion.div>
+						);
+					})}
+				</AnimatePresence>
+			</MotionBox>
 		</Container>
 	);
 };
