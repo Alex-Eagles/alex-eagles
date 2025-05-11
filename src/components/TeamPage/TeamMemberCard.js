cd76be77bb682432bacbe2154ebb5a0c60566222
John Ayman <139935256+JohnAyman12@users.noreply.github.com>
2025-05-11 23:20:07 +0300

    merge conflict solved

--- src/components/TeamPage/TeamMemberCard.js
+++ src/components/TeamPage/TeamMemberCard.js
@@ -1,115 +1,31 @@
-import { Card, CardContent, Typography, Avatar, Stack } from "@mui/material";
-import { LinkedIn, GitHub, EmailOutlined } from "@mui/icons-material";
-import useAnimate from "../../hooks/use-animate";
+import React from "react";
+import "./TeamMemberCard.css";
 
-const TeamMemberCard = (props) => {
-	const { name, role, image, email, linkedInLink, gitHubLink } = props;
-	const elementRef = useAnimate("animate", false);
-
+const TeamMemberCard = ({ name, role, image }) => {
 	return (
-		<Card
-			ref={elementRef}
-			sx={{
-				maxWidth: 150,
-				mx: window.innerWidth > 600 ? 4 : 0,
-				my: 4,
-				boxShadow: "none",
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				justifyContent: "center",
-				opacity: 0,
-				transition: "all 1s ease",
-			}}>
-			<Avatar
-				alt={name}
-				src={image}
-				sx={{ width: 150, height: 150, mx: "auto" }}
-			/>
-			<CardContent
-				sx={{
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-					justifyContent: "center",
-					textAlign: "center",
-				}}>
-				<Typography gutterBottom variant="h5" component="div">
-					{name}
-				</Typography>
-				<Typography variant="body2" color="text.secondary">
-					{role}
-				</Typography>
-				<Stack
-					direction="row"
-					spacing={1}
-					sx={{
-						mt: 2,
-						display: "flex",
-						flexDirection: "row",
-						alignItems: "center",
-						justifyContent: "center",
-					}}>
-					<a
-						href={email}
-						target="_blank"
-						rel="noopener noreferrer"
-						style={{
-							textDecoration: "none",
-							color: "inherit",
-						}}>
-						<EmailOutlined
-							sx={{
-								transform: "scale(1.15)",
-								transition: "all 0.5s ease",
-								"&:hover": {
-									transform: "translateY(-10%) scale(1.15)",
-									color: "#305fb0",
-								},
-							}}
-						/>
-					</a>
-					<a
-						href={gitHubLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						style={{
-							textDecoration: "none",
-							color: "inherit",
-						}}>
-						<GitHub
-							sx={{
-								transform: "scale(0.95)",
-								transition: "all 0.5s ease",
-								"&:hover": {
-									transform: "translateY(-10%) scale(0.95)",
-									color: "#305fb0",
-								},
-							}}
-						/>
-					</a>
-					<a
-						href={linkedInLink}
-						target="_blank"
-						rel="noopener noreferrer"
-						style={{
-							textDecoration: "none",
-							color: "inherit",
-						}}>
-						<LinkedIn
-							sx={{
-								transform: "scale(1.1)",
-								transition: "all 0.5s ease",
-								"&:hover": {
-									transform: "translateY(-10%) scale(1.1)",
-									color: "#305fb0",
-								},
-							}}
-						/>
-					</a>
-				</Stack>
-			</CardContent>
-		</Card>
+		<>
+			{/* <div class="container">
+				<img src={image} alt="Avatar" class="image" />
+				<div class="overlay">
+					<h3 style={{ margin: 0 }}>{name}</h3>
+					<p style={{ color: "#777" }}>{role}</p>
+				</div>
+			</div> */}
+			{/* <div class="container">
+				<img src={image} alt="Avatar" class="image" style={{ width: '100%' }}/>
+					<div class="middle">
+					<h3 style={{ margin: 0 }}>{name}</h3>
+					<p style={{ color: "#777" }}>{role}</p>
+					</div>
+			</div> */}
+			<div class="card">
+				<img src={image} alt="Avatar" style={{ width: '100%' }}	 />
+				<div class="container">
+					<h3 style={{ margin: 0 }}>{name}</h3>
+					<p style={{ color: "#777" }}>{role}</p>
+				</div>
+			</div>
+		</>
 	);
 };
 
--- src/pages/TeamPage.js
+++ src/pages/TeamPage.js
@@ -6,6 +6,10 @@ import { Box } from "@mui/material";
 import Footer from "../components/Footer/Footer";
 import Background from "../components/Background/Background";
 import TeamMembersSection from "../components/TeamPage/TeamMembersSection";
+import SectionHeading from "../components/SectionHeading/SectionHeading";
+import teamMemberData from "../assets/data/teamMemberData";
+
+const [heads, autonomusLead, mechanicalLead, autonomous, mechanical] = teamMemberData;
 
 const TeamPage = () => {
 	return (
@@ -47,7 +51,42 @@ const TeamPage = () => {
 						justifyItems: "center",
 						alignItems: "center",
 					}}>
-					<TeamMembersSection />
+
+					<SectionHeading
+						title="Our Team"
+						subtitle="Meet the visionaries who guide our mission, set our goals, and inspire the team to achieve excellence."
+						mainSection="true"
+					/>
+					<br />
+					<TeamMembersSection
+						title="Heads"
+						subtitle="Meet the visionaries who guide our mission, set our goals, and inspire the team to achieve excellence."
+						members={heads}
+					/>
+					<br />
+					<TeamMembersSection
+						title="Autonomus Lead"
+						subtitle="Leading the charge in innovation, our autonomous team heads push the boundaries of intelligence and automation."
+						members={autonomusLead}
+					/>
+					<br />
+					<TeamMembersSection
+						title="Mechanical Lead"
+						subtitle="At the forefront of engineering excellence, our mechanical heads transform imagination into mechanical reality."
+						members={mechanicalLead}
+					/>
+					<br />
+					<TeamMembersSection
+						title="Autonomous Team"
+						subtitle="The tech innovators coding, wiring, and empowering the brain behind our autonomous systems."
+						members={autonomous}
+					/>
+					<br />
+					<TeamMembersSection
+						title="Mechanical Team"
+						subtitle="The builders and makers who craft ideas into precision-engineered reality, shaping the future piece by piece."
+						members={mechanical}
+					/>
 				</Box>
 				<Footer />
 			</AnimatedPage>
