649cd00599c855897e1e92568b336b4fcc512c13
Ziad <ziadd.essam03@gmail.com>
2025-05-12 17:36:39 +0300

    add more media content for taco

--- src/assets/data/fixedwingMedia.js
+++ src/assets/data/fixedwingMedia.js
@@ -2,6 +2,7 @@ import flyingVideo from "../videos/fixed-wing-videos/FixedWingFlying.mp4"
 import fixedOnLand from "../videos/fixed-wing-videos/fixedWingOnLand.mp4"
 import section2Image from "../images/fixedwingImages/section2.jpg"
 import section3Image from "../images/fixedwingImages/section3.jpg"
+import section4Image from "../images/fixedwingImages/section4.jpg"
 // src/data/media.js
 export const mediaItems = [
   {
@@ -33,12 +34,21 @@ export const mediaItems = [
   {
     id: 4,
     category: " Structural Design ",
-    type: "video",
-    src: flyingVideo,
+    type: "image",
+    src: section4Image,
     title: "",
     caption: "With 10+ years of experience in UAV design the final form for Taco is drawn with finesse and optimized for minimum weight and high structural strength on Fusion 360 software. Manufacturing involved using balsa wood for the ribs, aluminum for the spar and tail boom and finally Monocote as skin. All materials chosen to satisfy the required structural strength while maintaining minimum weight."
     
   },
+  {
+    id: 5,
+    category: "Taco in action",
+    type: "video",
+    src: flyingVideo,
+    title: "",
+    caption: "Taco’s first flight was a success, achieving a stable flight.",
+  },
+ 
   
   // …and so on for each stage
 ];
